024eec55c6454ef5a2784fb08da503bef983a063
wakakusa <a.beginner.of.kdd.english@gmail.com>
2019-11-19 23:46:41 +0900

    test case change

--- test/initScoreTest.jl
+++ test/initScoreTest.jl
@@ -3,63 +3,37 @@
 # https://github.com/Microsoft/LightGBM/issues/1778
 # https://stackoverflow.com/questions/57275029/using-the-score-from-first-lightgbm-as-init-score-to-second-lightgbm-gives-diffe
 
-using BenchmarkTools
-# Create a 3-class problem
-N1 = 1000
-N2 = 1000
-N3 = 1000
-N = N1 + N2 + N3
+@testset "initScoreTest.jl" begin
+    try
+        # Test regression estimator.
+        regression_test = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/regression/regression.test", '\t');
+        regression_train = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/regression/regression.train", '\t');
+        X_train = regression_train[:, 2:end]
+        y_train = regression_train[:, 1]
+        X_test = regression_test[:, 2:end]
+        y_test = regression_test[:, 1]
 
-numClasses = 3
-numFeats = 5
+        regression_test_init = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/regression/regression.test.init", '\t')[:,1];
+        regression_train_init = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/regression/regression.train.init", '\t')[:,1];
 
-X = rand(N, numFeats)
-y = vcat(zeros(Int,N1), ones(Int,N2), 2*ones(Int,N3))
+        estimator = LightGBM.LGBMRegression(num_iterations = 100,
+                                            learning_rate = .05,
+                                            feature_fraction = .9,
+                                            bagging_fraction = .8,
+                                            bagging_freq = 5,
+                                            num_leaves = 31,
+                                            metric = ["l2"],
+                                            metric_freq = 1,
+                                            is_training_metric = true,
+                                            max_bin = 255,
+                                            min_sum_hessian_in_leaf = 5.,
+                                            min_data_in_leaf = 100,
+                                            max_depth = -1);
 
-estimator1 = LightGBM.LGBMMulticlass(num_iterations = 50,
-                                    learning_rate = .5,
-                                    feature_fraction = 0.5,
-                                    bagging_fraction = 1.0,
-                                    bagging_freq = 1,
-                                    num_leaves = 5,
-                                    metric = ["multi_logloss"],
-                                    num_class = numClasses,
-                                    min_data_in_leaf=1,
-                                    min_sum_hessian_in_leaf=1,
-                                    early_stopping_round = 1);
-                                    
-# using the incorrect size of init_score should throw
-estimator2=estimator1
-LightGBM.fit(estimator1, X, y)
-
-pre = LightGBM.predict(estimator1, X, verbosity = 0);
-a=@benchmark LightGBM.predict(estimator1, X, verbosity = 0)
-
-try
-#    LightGBM.fit(estimator2, X, y, init_score = Vector(rand(numClasses*N)))
-    LightGBM.fit(estimator2, X, y, init_score = Vector(pre))
-    post = LightGBM.predict(estimator2, X, verbosity = 0) +pre
-    b=@benchmark (LightGBM.predict(estimator2, X, verbosity = 0) +pre )
-    ratio( mean(a), mean(b) )
-    formattedclassfit(pre,X)
-    formattedclassfit(post,X)
-    
-    @test  
-    @test pre == post
-    
-    #false  # LightGBM.fit did not throw with incorrect init_score size
-catch
+        LightGBM.fit(estimator, X_train, y_train, verbosity = 0,init_score=regression_train_init);
+        LightGBM.fit(estimator, X_train, y_train, (X_test, y_test), verbosity = 0,init_score=regression_train_init);
+        @test true
+    catch
+        @test false
+    end
 end
-
-# Create init score as if it were already predicting very well the data
-init_score = -10000.0 * ones(numClasses, N)
-for i in 1:N
-    init_score[Int(y[i] + 1), i] *= -1
-end
-init_score = reshape(init_score, length(init_score))
-
-LightGBM.fit(estimator, X, y; init_score = init_score)
-
-# then it should have early stopped without training any tree
-@test LightGBM.LGBM_BoosterGetCurrentIteration(estimator1.booster) == 50
-
--- test/weightsTest.jl
+++ test/weightsTest.jl
@@ -1,65 +1,37 @@
-# Tests that the weighting scheme works for multiclass to balance skewed data
+# Tests that the weighting scheme works for binary_classification
 
-# Get margin for each sample in votes, according to its true label in labels
-function margin( votes::Array{T1,2}, labels::Vector{T2} ) where {T1 <: Real, T2 <: Real}
-    numClasses = size(votes,1)
-    numSamples = size(votes,2)
+@testset "weightsTest.jl" begin
+    try
+        binary_test = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/binary_classification/binary.test", '\t');
+        binary_train = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/binary_classification/binary.train", '\t');
+        X_train = binary_train[:, 2:end]
+        y_train = binary_train[:, 1]
+        X_test = binary_test[:, 2:end]
+        y_test = binary_test[:, 1]
 
-    votes = copy(votes)
+        binary_test_weight = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/binary_classification/binary.test.weight", '\t')[:,1];
+        binary_train_weight = readdlm(ENV["LIGHTGBM_PATH"] * "/examples/binary_classification/binary.train.weight", '\t')[:,1];
 
-    mg = zeros(eltype(votes), numSamples)
-    for i=1:numSamples
-        local v = votes[Int(labels[i]) + 1, i]  # copy score of true label
+        # Test binary estimator.
+        estimator = LightGBM.LGBMBinary(num_iterations = 20,
+                                        learning_rate = .1,
+                                        early_stopping_round = 1,
+                                        feature_fraction = .8,
+                                        bagging_fraction = .9,
+                                        bagging_freq = 1,
+                                        num_leaves = 1000,
+                                        metric = ["auc", "binary_logloss"],
+                                        is_training_metric = true,
+                                        max_bin = 255,
+                                        min_sum_hessian_in_leaf = 0.,
+                                        min_data_in_leaf = 1);
 
-        # compute margin
-        votes[Int(labels[i]) + 1, i] = -Inf
-        mg[i] = v - maximum(votes[:,i])
+        # Test fitting.
+        LightGBM.fit(estimator, X_train, y_train, weights=binary_train_weight[:,1]);
+        LightGBM.fit(estimator, X_train, y_train, (X_test, y_test), weights=binary_train_weight[:,1]); 
+
+        @test true
+    catch
+        @test false
     end
-
-    return mg
-end
-
-# Create a skewed 3-class problem
-N1 = 100
-N2 = 100000
-N3 = 1000000
-N = N1 + N2 + N3
-
-numClasses = 3
-numFeats = 5
-
-X = rand(N, numFeats)
-y = vcat(zeros(N1), ones(N2), 2*ones(N3))
-
-# one feature is exactly the label
-X[:,1] = y
-
-estimator = LightGBM.LGBMMulticlass(num_iterations = 10,
-                                    learning_rate = .5,
-                                    feature_fraction = 0.5,
-                                    bagging_fraction = 1.0,
-                                    bagging_freq = 1,
-                                    num_leaves = 5,
-                                    metric = ["multi_logloss"],
-                                    num_class = numClasses,
-                                    min_data_in_leaf=1,
-                                    min_sum_hessian_in_leaf=1);
-
-# Compute required weights
-classWeights = [N / (numClasses * sum(y .== n)) for n in 0:(numClasses - 1)]
-weights = [classWeights[Int(n+1)] for n in y]
-
-LightGBM.fit(estimator, X, y; weights=weights);
-
-pred = LightGBM.predict(estimator, X; predict_type=1)
-pred = reshape(pred, ( numClasses, size(X,1)))
-
-# Average margins for each prediction should be similar if weighting works
-margins = margin(pred, y)
-margins = [StatsBase.mean(margins[y .== label]) for label in 0:(numClasses-1)]
-
-meanMargin = StatsBase.mean(margins)
-marginDeviations = abs.(map(x->x-meanMargin,margins))
-maxMarginDeviation = maximum(marginDeviations)
-
-@test maxMarginDeviation / meanMargin < 0.1
+end   
