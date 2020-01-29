f924700639733b601708d3b766a3231443d0404a
Yaqub Alwan <yaqub.alwan@iqvia.com>
2020-01-29 08:29:25 +0000

    adding a few more tests

--- test/ffi/datasets.jl
+++ test/ffi/datasets.jl
@@ -6,12 +6,16 @@ using Test
 # we don't want the LightGBM vom
 # redirect_stdout()
 
+verbosity = "verbose=-1"
+
 @testset "LGBM_DatasetCreateFromMat -- floats/column major" begin
 
     mymat = [1. 2.; 3. 4.; 5. 6.]
-    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, "min_data_in_leaf=1")
+    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, verbosity)
 
     @test created_dataset.handle != C_NULL
+    @test LightGBM.LGBM_DatasetGetNumData(created_dataset) == 3
+    @test LightGBM.LGBM_DatasetGetNumFeature(created_dataset) == 2
 
     @test LightGBM.LGBM_DatasetSetFeatureNames(created_dataset, ["a", "b"]) == nothing
     # cause its not row major and has 2 cols
@@ -22,10 +26,13 @@ end
 @testset "LGBM_DatasetCreateFromMat -- floats/row major" begin
 
     mymat = [1. 2.; 3. 4.; 5. 6.]
-    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, "min_data_in_leaf=1", true)
+    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, verbosity, true)
 
     @test created_dataset.handle != C_NULL
+    @test LightGBM.LGBM_DatasetGetNumData(created_dataset) == 2
+    @test LightGBM.LGBM_DatasetGetNumFeature(created_dataset) == 3
 
+    # this kind of stuff is a bit circular but without a better way to interact with a dataset, its hard to test
     @test_throws ErrorException LightGBM.LGBM_DatasetSetFeatureNames(created_dataset, ["a", "b"])
     # cause its row major with 2 initial cols and 3 rows
     @test LightGBM.LGBM_DatasetSetFeatureNames(created_dataset, ["a", "b", "c"]) == nothing
@@ -35,9 +42,11 @@ end
 @testset "LGBM_DatasetCreateFromMat -- ints/column major" begin
 
     mymat = [1 2; 3 4; 5 6]
-    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, "min_data_in_leaf=1")
+    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, verbosity)
 
     @test created_dataset.handle != C_NULL
+    @test LightGBM.LGBM_DatasetGetNumData(created_dataset) == 3
+    @test LightGBM.LGBM_DatasetGetNumFeature(created_dataset) == 2
 
     @test LightGBM.LGBM_DatasetSetFeatureNames(created_dataset, ["a", "b"]) == nothing
     # cause its not row major and has 2 cols
@@ -48,9 +57,11 @@ end
 @testset "LGBM_DatasetCreateFromMat -- ints/row major" begin
 
     mymat = [1 2; 3 4; 5 6]
-    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, "min_data_in_leaf=1", true)
+    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, verbosity, true)
 
     @test created_dataset.handle != C_NULL
+    @test LightGBM.LGBM_DatasetGetNumData(created_dataset) == 2
+    @test LightGBM.LGBM_DatasetGetNumFeature(created_dataset) == 3
 
     @test_throws ErrorException LightGBM.LGBM_DatasetSetFeatureNames(created_dataset, ["a", "b"])
     # cause its row major with 2 initial cols and 3 rows
@@ -58,5 +69,26 @@ end
 
 end
 
+
+@testset "LGBM_DatasetGetSubset" begin
+
+    mymat = [1. 2.; 3. 4.; 5. 6.]
+    created_dataset = LightGBM.LGBM_DatasetCreateFromMat(mymat, verbosity)
+
+    get_idx = Int32.([1, 3]) # check julia indices are correctly translated
+    # gotta create the same so its not a reference, so we can check the mutation is correct (or at least it looks like it hasn't been mutated)
+    get_idx_copy = Int32.([1, 3])
+    faulty_idx = Int32.([1, 5])
+    # this isn't a julia matrix -- its another dataset
+    fetched = LightGBM.LGBM_DatasetGetSubset(created_dataset, get_idx, verbosity)
+
+    # check it isn't the same dataset...
+    @test fetched.handle != created_dataset.handle
+    @test get_idx == get_idx_copy
+
+    @test_throws ErrorException LightGBM.LGBM_DatasetGetSubset(created_dataset, faulty_idx, verbosity)
+
+
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 
-@testset "LightGBM.jl" begin
+using Test
+
+
+@testset "FFI unit tests" begin
+
+    @testset "Dataset" begin
+        include(joinpath("ffi", "datasets.jl"))
+    end
+
+end
+
+
+@testset "Integration tests" begin
 
     @testset "Basic Tests" begin
         include("basic_tests.jl")
@@ -11,14 +23,6 @@
 
     @testset "Init Score Test" begin
         include("initScoreTest.jl")
-    end
-
-end
-
-@testset "FFI" begin
-
-    @testset "Dataset" begin
-        include(joinpath("ffi", "datasets.jl"))l
     end
 
 end
