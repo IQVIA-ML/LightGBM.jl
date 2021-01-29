86edd41faf442bbdfbd9b1ac5ae20105f8b08c5f
chilledgeek <chilledgeek@gmail.com>
2021-01-29 16:07:03 +0000

    add truncate_booster to mlj interface

---
+++ test/basic/test_mljinterface.jl
@@ -0,0 +1,44 @@
+module TestUtils
+
+
+using MLJBase
+using Test
+
+import LightGBM
+
+
+@testset "mlj_to_kwargs removes classifier truncate_booster flag" begin
+    # Arrange
+    fixture = LightGBM.MLJInterface.LGBMClassifier()
+
+    # Act
+    output = LightGBM.MLJInterface.mlj_to_kwargs(fixture)
+
+    # Assert
+    @test :truncate_booster ∉ keys(output)
+end
+
+@testset "mlj_to_kwargs removes regressor truncate_booster flag" begin
+    # Arrange
+    fixture = LightGBM.MLJInterface.LGBMRegressor()
+
+    # Act
+    output = LightGBM.MLJInterface.mlj_to_kwargs(fixture)
+
+    # Assert
+    @test :truncate_booster ∉ keys(output)
+end
+
+@testset "mlj_to_kwargs adds classifier num_class" begin
+    # Arrange
+    fixture = LightGBM.MLJInterface.LGBMClassifier()
+
+    # Act
+    output = LightGBM.MLJInterface.mlj_to_kwargs(fixture, [0,1])
+
+    # Assert
+    @test :num_class in keys(output)
+end
+
+
+end # Module
