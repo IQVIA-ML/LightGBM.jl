a901c6ecc871ea822fa376cb35bee6ee9a0359ca
Tahavori <u1104634@gblon4wlz40602.rxcorp.com>
2022-07-29 01:02:49 +0430

    Fix dw the binaries

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,19 @@
 
 using Libdl
 
-savepath = normpath(joinpath(@__DIR__, "..", "src", "lib_lightgbm.$(Libdl.dlext)"))
+current_file_path = @__DIR__
+savepath = normpath(joinpath(current_file_path, "..", "src"))
+
+if !isdir(savepath)
+    mkdir(savepath)
+end
+
+cd(savepath)
+
 
 # These precompiled ones are CPU-only, can incorporate stuff later for user-built GPU capable binaries
 GH_URL = "https://github.com/microsoft/LightGBM/releases/download/v3.3.2/lib_lightgbm.$(Libdl.dlext)"
 
-download(GH_URL, savepath)
-
+download(GH_URL, "lib_lightgbm.$(Libdl.dlext)")
+cd(current_file_path)
 # The end
