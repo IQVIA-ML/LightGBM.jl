094eb0b2c13fd068be39d796860820fd9b86416a
Yaqub Alwan <yaxxie@users.noreply.github.com>
2022-08-23 23:56:06 +0100

    reducing the amount of assumptions in the tests of find_library

--- test/basic/test_lightgbm.jl
+++ test/basic/test_lightgbm.jl
@@ -6,19 +6,36 @@ using Test
 
 src_dir = abspath(joinpath(@__DIR__, "..", "..", "src"))
 
+# little helper to get base libnames
+libname(x) = first(splitext(basename(x)))
+
 # These set of tests use common libraries of each system to test `find_library` without having to modify env variables
 function setup_env()
+
+    # get libs that are definitely present and linkable for tests (with just system extension)
+    # by virtue of being loaded
+    loaded = Libdl.dllist()
+    # get all the ones which just have the system extension and no funny business
+    withext = loaded[endswith.(loaded, Ref(Libdl.dlext))]
+    # get the libname without prefix and extension
+    withoutext = libname.(withext)
+    # check which are loadable (or findable by find_library, this is WILD if they're not all found)
+    libnames = Libdl.find_library.(withoutext)
+    # get the first one matching or nothing
+    libidx = findfirst(libnames .== withoutext)
+    borked = libidx == nothing
+
+
+
     output = Dict()
+    output["sample_lib"] = ""
+    output["linkable_path"] = ""
 
-    if Sys.islinux()
-        output["sample_lib"] = "libcrypt"
-    elseif Sys.isunix()
-        output["sample_lib"] = "libpython"
-    elseif Sys.iswindows()
-        output["sample_lib"] = "netmsg"
+    if !borked
+        output["sample_lib"] = withoutext[libidx]
+        # fullpath of a linkable lib to copy off the sys path
+        output["linkable_path"] = withext[libidx]
     end
-
-    output["ref_lib_lightgbm_path"] = joinpath(src_dir, "lib_lightgbm.$(Libdl.dlext)")
 
     # where to create a fixture library file (custom path) where such library exists in the syspath
     output["custom_fixture_path"] = joinpath(src_dir, "$(output["sample_lib"]).$(Libdl.dlext)")
@@ -26,7 +43,7 @@ function setup_env()
     # where to create a fixture library file (custom path) where such library does NOT exist in the syspath
     output["lib_not_on_sys_fixture_path"] = joinpath(src_dir, "lib_not_on_sys.$(Libdl.dlext)")
 
-    return output
+    return output, borked
 
 end
 
@@ -44,14 +61,20 @@ end
     @testset "find_library works with no system lib" begin
 
         # Arrange
-        settings = setup_env()
-        cp(settings["ref_lib_lightgbm_path"], settings["lib_not_on_sys_fixture_path"]) # fake file copied from lightgbm
+        settings, isborked = setup_env()
+        if !isborked
+            cp(settings["linkable_path"], settings["lib_not_on_sys_fixture_path"], force=true, follow_symlinks=true)
+        end
 
         # Act
         output = LightGBM.find_library("lib_not_on_sys", [src_dir])
 
         # Assert
-        @test output == joinpath(src_dir, "lib_not_on_sys") # custom path detected (without extension)
+        if !isborked
+            @test output == joinpath(src_dir, "lib_not_on_sys") # custom path detected (without extension)
+        else
+            @test_broken output == joinpath(src_dir, "lib_not_on_sys")
+        end
 
         teardown(settings)
     end
@@ -59,14 +82,20 @@ end
     @testset "find_library finds system lib first" begin
 
         # Arrange
-        settings = setup_env()
-        cp(settings["ref_lib_lightgbm_path"], settings["custom_fixture_path"]) # fake file copied from lightgbm
+        settings, isborked = setup_env()
+        if !isborked
+            cp(settings["linkable_path"], settings["custom_fixture_path"], force=true, follow_symlinks=true)
+        end
 
         # Act
         output = LightGBM.find_library(settings["sample_lib"], [src_dir])
 
         # Assert
-        @test output == settings["sample_lib"] # sys lib detected
+        if !isborked
+            @test output == settings["sample_lib"] # sys lib detected
+        else
+            @test_broken output == settings["sample_lib"]
+        end
 
         teardown(settings)
     end
@@ -74,7 +103,7 @@ end
     @testset "find_library finds system lib" begin
 
         # Arrange
-        settings = setup_env()
+        settings, isborked = setup_env()
 
         # Act
         output = LightGBM.find_library(settings["sample_lib"], [src_dir]) # library should only exist in syspath, not custom path
@@ -88,7 +117,7 @@ end
     @testset "find_library returns empty and logs error" begin
 
         # Arrange
-        settings = setup_env()
+        settings, _ = setup_env()
 
         # Act and assert
         @test_throws LightGBM.LibraryNotFoundError LightGBM.find_library("lib_that_simply_doesnt_exist", [src_dir])
