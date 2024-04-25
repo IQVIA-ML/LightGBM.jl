5d21236960c668504f5c0af8b16df2c7e41cbf48
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-04-25 10:36:53 -0500

    feat: use LightGBM_jll instead of precompiled binaries (#134)

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,10 @@
-
 using Libdl
 
-savepath = normpath(joinpath(@__DIR__, "..", "src", "lib_lightgbm.$(Libdl.dlext)"))
+if (VERSION ∈ (v"1.8.4", v"1.8.5")) && Sys.iswindows()
+    # See https://github.com/JuliaLang/julia/issues/48187
 
-# These precompiled ones are CPU-only, can incorporate stuff later for user-built GPU capable binaries
-GH_URL = "https://github.com/microsoft/LightGBM/releases/download/v3.3.5/lib_lightgbm.$(Libdl.dlext)"
-
-download(GH_URL, savepath)
-
-# The end
+    # LightGBM_jll UUID
+    savepath = normpath(joinpath(@__DIR__, "..", "src", "lib_lightgbm.$(Libdl.dlext)"))
+    GH_URL = "https://github.com/microsoft/LightGBM/releases/download/v3.3.5/lib_lightgbm.$(Libdl.dlext)"
+    download(GH_URL, savepath)
+end
