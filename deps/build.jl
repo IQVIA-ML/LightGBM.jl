28fe7288733409b2c9756b6502c963eebfe1a5ea
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-04-25 05:05:50 -0500

    fix: use static version number

--- deps/build.jl
+++ deps/build.jl
@@ -1,14 +1,10 @@
 using Libdl
-import Pkg
-import UUIDs
 
 if (VERSION ∈ (v"1.8.4", v"1.8.5")) && Sys.iswindows()
     # See https://github.com/JuliaLang/julia/issues/48187
 
     # LightGBM_jll UUID
-    JLL_version = Pkg.dependencies()[UUID("0e4427ef-1ff7-5cd7-8faa-8ff0877bb2ec")].version
-    version_string = "$(JLL_version.major).$(JLL_version.minor).$(JLL_version.patch)"
     savepath = normpath(joinpath(@__DIR__, "..", "src", "lib_lightgbm.$(Libdl.dlext)"))
-    GH_URL = "https://github.com/microsoft/LightGBM/releases/download/v$version_string/lib_lightgbm.$(Libdl.dlext)"
+    GH_URL = "https://github.com/microsoft/LightGBM/releases/download/v3.3.5/lib_lightgbm.$(Libdl.dlext)"
     download(GH_URL, savepath)
 end
